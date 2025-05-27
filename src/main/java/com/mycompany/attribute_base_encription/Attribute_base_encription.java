87465ac67de6989f7721741480176c07f97fa238
Niyazahmed27 <ahmedniyaz532@gmail.com>
2025-05-27 13:59:23 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/attribute_base_encription/Attribute_base_encription.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.attribute_base_encription;
+
+/**
+ *
+ * @author hiiqm
+ */
+public class Attribute_base_encription {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
